2ad1ac8cbded2c61b9635e420699184084da2c18
LUCAS HENRIQUE DOS SANTOS <86979899+LucasSantosTaqua2@users.noreply.github.com>
2025-05-22 21:17:30 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ from sqlalchemy.orm import Session
 
 # CORRIGIDO: Importar Base e garantir que engine está correto
 
-from app.core.database import get\_session, engine, Base \# create\_db\_and\_tables não é mais necessária aqui se chamamos Base.metadata.create\_all
+from app.core.database import get_session, engine, Base# create\_db\_and\_tables não é mais necessária aqui se chamamos Base.metadata.create\_all
 from app.core.config import settings
-from app.crud.user import create\_user, get\_user\_by\_username
-from app.models.user import User, UserRole \# Modelos SQLAlchemy, UserRole agora terá valores MAIÚSCULOS
+from app.crud.user import create_user, get_user_by_username
+from app.models.user import User, UserRole # Modelos SQLAlchemy, UserRole agora terá valores MAIÚSCULOS
 from app.schemas.user import UserCreate
 
 app = FastAPI(
